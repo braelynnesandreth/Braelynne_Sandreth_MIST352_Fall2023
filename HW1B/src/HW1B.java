b39af64ca5fdb63b2b93a05dc15342584b3f49c7
braelynnesandreth <braelynnesandreth@localhost>
2023-09-07 17:37:44 -0400

    Submitted HW#1

---
+++ HW1B/src/HW1B.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+
+public class HW1B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		// Ask the user for input
+	        System.out.print("Enter value for num1: ");
+	        double num1 = scanner.nextDouble();
+	        
+	        System.out.print("Enter value for num2: ");
+	        double num2 = scanner.nextDouble();
+	        
+	        // Perform calculations
+	        double sumResult = num1 + num2;
+	        double differenceResult = num1 - num2;
+	        double productResult = num1 * num2;
+	        double quotientResult = num1 / num2;
+	        
+	     // Display the results
+	        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sumResult);
+	        System.out.println("Difference of " + num1 + " and " + num2 + " is " + differenceResult);
+	        System.out.println("Product of " + num1 + " and " + num2 + " is " + productResult);
+	        System.out.println("Quotient of " + num1 + " and " + num2 + " is " + quotientResult);
+
+	        // Close the scanner
+	        scanner.close();
+	        
+	      
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		// TODO Auto-generated method stub
+
+	}
+
+}
